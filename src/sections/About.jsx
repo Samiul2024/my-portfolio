56d0920bde9429240a641ecf98eb34405da2d48b
Samiul2024 <samiullahossen@gmail.com>
2025-07-16 22:04:34 +0600

    added title

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,7 @@
 import samiul from '../../public/samiul.jpg';
 export default function About() {
+
+  const name=<h3 className='font-semibold text-3xl'>MD. Samiulla Hossen</h3>
   return (
     <div className="text-center p-6">
        <img
@@ -7,9 +9,9 @@ export default function About() {
         alt="Md. Samiulla Hossen"
         className="mx-auto w-40 h-48 rounded-full border-4 border-blue-500 shadow-lg hover:scale-105 transition-transform duration-300"
       />
-      <h2 className="text-3xl font-bold mb-4">About Me</h2>
+      <h2 className="text-3xl font-bold mb-4">Hi,</h2>
       <p>
-        I’m a MERN Stack Front-End Developer passionate about building fast, accessible, and beautiful web apps.
+       I'm{name} a MERN Stack Front-End Developer passionate about building fast, accessible, and beautiful web apps.
       </p>
     </div>
   );
