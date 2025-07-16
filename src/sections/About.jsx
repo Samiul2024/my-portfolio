eaf663216bc1e33db000cb7344639fb1cdc4e88a
Samiul2024 <samiullahossen@gmail.com>
2025-07-16 18:21:12 +0600

    added my photo

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,12 @@
+import samiul from '../../public/samiul.jpg';
 export default function About() {
   return (
     <div className="text-center p-6">
+       <img
+        src={samiul}
+        alt="Md. Samiulla Hossen"
+        className="mx-auto w-40 h-48 rounded-full border-4 border-blue-500 shadow-lg hover:scale-105 transition-transform duration-300"
+      />
       <h2 className="text-3xl font-bold mb-4">About Me</h2>
       <p>
         I’m a MERN Stack Front-End Developer passionate about building fast, accessible, and beautiful web apps.
